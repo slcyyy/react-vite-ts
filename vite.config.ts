ce91af2fd03bea11a5a835cae57137cc866314f9
luochun <luochun@idcos.com>
2023-03-16 17:59:21 +0800

    feat: 设置项目默认端口

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,14 @@
-import { defineConfig } from 'vite';
+import { defineConfig, loadEnv } from 'vite';
 import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [react()],
+export default defineConfig(({ mode }) => {
+  // vite默认不加载.env文件的，因为这些文件需要在执行完 Vite 配置后才能确定加载哪一个
+  const env = loadEnv(mode, process.cwd(), '');
+  return {
+    plugins: [react()],
+    server: {
+      port: Number(env.VITE_DEV_PORT),
+    },
+  };
 });
